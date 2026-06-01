c11ced13ce9ca6787b4c0936500a680aac1be03d
Matias Muñoz~ <san2002fire@gmail.com>
2026-06-01 20:09:02 -0300

    Ej3 echo

--- Basicos/Codigos/TP1/Ej1.cs
+++ Basicos/Codigos/TP1/Ej1.cs
@@ -8,3 +8,17 @@ class Program
         Console.WriteLine("Hola, Mundo");
     }
 }
+
+//1. ¿Desde qué parte comienza a ejecutarse el programa?
+// Comienza a ejecutarse desde el método Main(), que es el punto de inicio del programa.
+
+//2. ¿Qué instrucción muestra información por pantalla?
+// a instrucción Console.WriteLine("Hola, mundo"); muestra texto en la pantalla.
+
+//3. ¿Para qué se usan las llaves { }?
+//Las llaves se usan para agrupar bloques de código y 
+//definir qué instrucciones pertenecen a una clase o a un método.
+
+//4. ¿Qué pasaría si se borra el punto y coma final de la línea Console.WriteLine?
+// El programa daría un error de compilación porque en C# las instrucciones deben
+//terminar con punto y coma (;).
---
+++ Basicos/Codigos/TP1/Ej2.cs
@@ -0,0 +1,14 @@
+//Reconocer la estructura básica de C#
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Inicio de Partida");
+        Console.WriteLine("Jugador: "+"Ama");
+        Console.WriteLine("Vidas: 3");
+        Console.WriteLine("Monedas: "+ "10");
+        Console.WriteLine("Fin de Resumen");
+    }
+}
