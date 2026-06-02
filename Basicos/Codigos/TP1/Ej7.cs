541767091403f703bc4c101a061c743b3d007d5a
Matias Muñoz~ <san2002fire@gmail.com>
2026-06-01 22:37:37 -0300

    Ej7 echo

---
+++ Basicos/Codigos/TP1/Ej7.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string nombrePersonaje= "Alex";
+        int nivel= 10;
+        int vidas=3;
+        int energia= 7;
+        bool tieneEscudo= true;
+        string poderEspecial= "Volar";
+
+        Console.WriteLine("Nombre del Personajes: "+nombrePersonaje);
+        Console.WriteLine("Nivel: "+nivel);
+        Console.WriteLine("Vidas: "+vidas);
+        Console.WriteLine("Energia: "+energia);
+        Console.WriteLine("Tiene Escudo?: "+ tieneEscudo);
+        Console.WriteLine("Poder Especial: "+ poderEspecial);
+
+    }
+}
